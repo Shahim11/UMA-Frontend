8671daafaa4aee3e56b380f0608859a42d6a510e
Shahim Uddin <Shahim6872@gmail.com>
2024-09-19 13:11:12 +0600

    second commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+// Base URL without `/api` path
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_API_URL,  // Use the environment variable
+    baseURL: process.env.REACT_APP_BACKEND_API_URL,  // Set this to just http://localhost:5000
+});
+
+// You can append '/api' globally like this, so you don’t need to put it in every request
+api.interceptors.request.use(config => {
+    config.url = `/api${config.url}`;
+    return config;
 });
 
 export default api;
